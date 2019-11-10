7a57004c7c449b4d1c10734d04bd33f3a5f647db
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-10 19:04:07 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,8 +7,8 @@ class Project
   end 
   
   def add_backer(backer)
-    @backers.push(backer)
-    # Backer.backed_projects.push(self)
+    backer_obj = Backer.new(backer)
+    self.backer = backer_obj
   end
 
 end 
